39fe2d5faed76b2c9af1240fc75f590005d86864
Sibo Wu <wusibo2020@hotmail.com>
2023-11-13 20:34:04 -0800

    except for extra credit

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
 import cors from "cors";
+import ModuleRoutes from "./modules/routes.js";
+
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+CourseRoutes(app);
 app.use(express.json());
+ModuleRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(8000);
