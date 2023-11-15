301389a27b8afb92d12f940c21d18d0e15b44a7a
Sibo Wu <wusibo2020@hotmail.com>
2023-11-15 14:08:43 -0800

    fix import in es building

--- Database/index.js
+++ Database/index.js
@@ -1,14 +1,26 @@
-import courses from "./courses.json" assert { type: "json" };
-import modules from "./modules.json" assert { type: "json" };
-import assignments from "./assignments.json" assert { type: "json" };
-import users from "./users.json" assert { type: "json" };
-import grades from "./grades.json" assert { type: "json" };
-import enrollments from "./enrollments.json" assert { type: "json" };
+import fs from 'fs';
+import path from 'path';
+
+const dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const readJsonFileSync = (filepath, encoding = 'utf8') => {
+    // Correct path for different OS
+    const correctPath = path.join(dirname, filepath).replace(/^(\w:\\)/, '/');
+    return JSON.parse(fs.readFileSync(correctPath, encoding));
+};
+
+const courses = readJsonFileSync('./courses.json');
+const modules = readJsonFileSync('./modules.json');
+const assignments = readJsonFileSync('./assignments.json');
+const users = readJsonFileSync('./users.json');
+const grades = readJsonFileSync('./grades.json');
+const enrollments = readJsonFileSync('./enrollments.json');
+
 export default {
-  courses,
-  modules,
-  assignments,
-  users,
-  grades,
-  enrollments,
+    courses,
+    modules,
+    assignments,
+    users,
+    grades,
+    enrollments,
 };
