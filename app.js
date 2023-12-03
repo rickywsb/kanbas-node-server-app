05c47172e26bb961ca6cd8fcec280cb4547e74c9
Sibo Wu <wusibo2020@hotmail.com>
2023-12-02 17:34:29 -0800

    fix b-f

--- app.js
+++ app.js
@@ -1,71 +1,70 @@
 import express from 'express';
 import session from "express-session";
+import cors from "cors";
+import mongoose from 'mongoose';
 import "dotenv/config";
+
+// Import routes
 import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
-import cors from "cors";
-import mongoose from 'mongoose';
 import UserRoutes from "./users/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from './assignments/routes.js';
 
+// Ensure required environment variables are set
+const requiredEnv = ['FRONTEND_URL', 'MONGODB_URI', 'SESSION_SECRET', 'PORT'];
+requiredEnv.forEach(envVar => {
+  if (!process.env[envVar]) {
+    console.error(`Missing required environment variable: ${envVar}`);
+    process.exit(1);
+  }
+});
 
-mongoose.connect("mongodb+srv://wusiboricky:zyq19960123@web5610.duup1ki.mongodb.net/kanbas");
+// Setup MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .catch(err => {
+    console.error('MongoDB connection error:', err.message);
+    process.exit(1);
+  });
 
 const app = express();
-app.use(
-    cors({
-      credentials: true,
-      origin: process.env.FRONTEND_URL
-    })
-   );
 
+// CORS configuration
+app.use(cors({
+  credentials: true,
+  origin: process.env.FRONTEND_URL
+}));
+
+// Session configuration
 const sessionOptions = {
-    secret: "any string",
-    resave: false,
-    saveUninitialized: false,
-  };
-  if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-    };
-  }
-  app.use(session(sessionOptions));
-  
-  
-// app.use(cors({
-//     credentials: true,
-//     origin: function (origin, callback) {
-//         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     }
-// }));
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {}
+};
+
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1); // Trust first proxy
+  sessionOptions.cookie.secure = true; // Serve secure cookies
+  sessionOptions.cookie.sameSite = 'none';
+}
+
+app.use(session(sessionOptions));
+
+// Middleware for JSON payload parsing
 app.use(express.json());
-// const allowedOrigins = ['http://localhost:3000', 'https://a5--creative-nougat-9b83f1.netlify.app'];
 
-// app.use(cors({
-//     credentials: true,
-//     origin: function (origin, callback) {
-//         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     }
-// }));
-
+// Route setup
 UserRoutes(app);
-
 CourseRoutes(app);
-
 ModuleRoutes(app);
 AssignmentRoutes(app);
 Lab5(app);
 Hello(app);
-app.listen(process.env.PORT ||8000);
+
+// Start server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
